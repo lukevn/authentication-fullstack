32a86790708a17beeb77df7406488629bb49e262
Luke <tuananh95dhtb@gmail.com>
2022-11-18 11:12:39 +0700

    feat: [BACKEND] add swagger api documentation

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,2 +1,5 @@
 // Environment Variables
 export const APP_PORT = process.env.APP_PORT;
+export const APP_ENV = process.env.APP_ENV;
+export const APP_DEBUG = process.env.APP_DEBUG;
+export const APP_VERSION = process.env.APP_VERSION;
